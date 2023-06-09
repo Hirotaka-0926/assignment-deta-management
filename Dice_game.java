1eb6d3d68c30bd377c9fcc1dabc1913ff5c3603d
Hirotaka-0926 <abehirotaka94@gmail.com>
2023-06-09 16:39:56 +0900

    Won/lost message added

--- Dice_game.java
+++ Dice_game.java
@@ -1,14 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class Dice_game{
     public static void main(String[] args){
-        Random rand= new Random();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name=sc.next();
+
+        System.out.println("Hello , "+name);
+        Random rand = new Random();
         int sum;
-        int Dice1=rand.nextInt(6)+1;
-        int Dice2=rand.nextInt(6)+1;
-        sum=Dice1+Dice2;
-        System.out.println("Die 1:"+Dice1);
-        System.out.println("Die 2:"+Dice2);
-        System.out.println("Total calue:"+sum);
+        int Die1=rand.nextInt(6)+1;
+        int Die2=rand.nextInt(6)+1;
+        sum=Die1+Die2;
+        System.out.println("Die 1: "+Die1);
+        System.out.println("Die 2: "+Die2);
+        System.out.println("total Value "+sum);
+
+        if(sum<=7)System.out.println(name+" lose");
+        else System.out.println(name+" won");
+        
     }
 }
