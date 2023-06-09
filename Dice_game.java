684c94ce0bcc96a4e41132b34e261fdf00e9e1a2
Hirotaka-0926 <abehirotaka94@gmail.com>
2023-06-09 16:10:43 +0900

    Greetings added

--- Dice_game.java
+++ Dice_game.java
@@ -2,13 +2,13 @@ import java.util.Random;
 
 class Dice_game{
     public static void main(String[] args){
-        Random rand= new Random();
+        Random rand = new Random();
         int sum;
-        int Dice1=rand.nextInt(6)+1;
-        int Dice2=rand.nextInt(6)+1;
-        sum=Dice1+Dice2;
-        System.out.println("Die 1:"+Dice1);
-        System.out.println("Die 2:"+Dice2);
-        System.out.println("Total calue:"+sum);
+        int Die1=rand.nextInt(6)+1;
+        int Die2=rand.nextInt(6)+1;
+        sum=Die1+Die2;
+        System.out.println("Die 1: "+Die1);
+        System.out.println("Die 2: "+Die2);
+        System.out.println("total Value"+sum);
     }
 }
